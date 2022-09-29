579fdd0affd2439a268a554fb45779fee2849250
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-09-29 19:33:50 -0400

    pulling main

--- controllers/recipes_controller.js
+++ controllers/recipes_controller.js
@@ -2,18 +2,18 @@ const { Router } = require('express')
 const express = require('express')
 const router = express.Router()
 const db = require("../models")
-const {recipe} = db
+const { recipe } = db
 
 // INDEX
 router.get('/', (req, res) => {
   res.render('show')
-  })
+})
 
 // router.get('/:id',  (req,res) => {
 
 // });
 // router.post('/',  (req, res) => {
- 
+
 // });
 
 // // UPDATE A BAND
@@ -23,7 +23,7 @@ router.get('/', (req, res) => {
 
 // // DELETE A BAND
 // router.delete('/:id',  (req, res) => {
- 
+
 // });
 
 module.exports = router
