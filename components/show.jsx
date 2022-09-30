52fa651240c05aa76cf45514b47b9bad71ea1af8
Zachary Dowell <zdowell97@gmail.com>
2022-09-29 21:01:30 -0400

    restructure

--- components/show.jsx
+++ components/show.jsx
@@ -5,11 +5,13 @@ import Def from './default'
 function show() {
     return (
         <Def>
+            <body>
             <h1>Show Page</h1>
             <h1>description</h1>
             <h1>ingredients</h1>
             <h1>image</h1>
             <h1>comments</h1>
+            </body>
         </Def>
     )
 }
---
+++ src/app.js
@@ -0,0 +1,13 @@
+import './App'
+function App () {
+    return (
+            <main>
+                <div>
+                    <h1>HOME PAGE</h1>
+                </div>
+            </main>
+       
+    )
+}
+
+export default App
