64e1f3579f40556ddb4385fb6cce174f26b8e95a
Zachary Dowell <zdowell97@gmail.com>
2022-09-29 21:18:44 -0400

    Revert "Update comment.js"
    This reverts commit c8b41e14968bb9391d6e0c2ffa1743d8f3cfa91a.

--- models/comment.js
+++ models/comment.js
@@ -9,42 +9,35 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ Recipe }) {
+    static associate(models) {
       // define association here
-
-      Comment.belongsTo(Recipe, {
-        foreignKey: "recipe_id",
-        as: "recipe"
-      })
     }
   }
   Comment.init({
     comment_id: {
-      type: DataTypes.INTEGER,
+      types: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true
     },
     recipe_id: {
-      type: DataTypes.INTEGER,
+      types: DataTypes.INTEGER,
       allowNull: false
     },
     author: {
-      type: DataTypes.STRING,
+      types: DataTypes.STRING,
       allowNull: false
     },
     comment_text: {
-      type: DataTypes.TEXT,
+      types: DataTypes.TEXT,
       allowNull: false
     },
     likes: {
-      type: DataTypes.INTEGER,
+      types: DataTypes.INTEGER,
       allowNull: false
     },
   }, {
     sequelize,
     modelName: 'Comment',
-    tableName: 'comments',
-    timestamps: false
   });
   return Comment;
 };
