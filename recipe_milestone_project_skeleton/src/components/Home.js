2be2cff6809ffc9687352e1acdf50a7a30a15a93
B-loeb <bradleyloeb@gmail.com>
2022-10-02 11:15:50 -0400

    stock images/colapse added to nav

--- recipe_milestone_project_skeleton/src/components/Home.js
+++ recipe_milestone_project_skeleton/src/components/Home.js
@@ -1,8 +1,15 @@
 function Home() {
     return (
         <div className="Home">
-          <h1>Home Page</h1>
+          <h1>Tastes of Home</h1>
+          <body> 
+          <p>add images here</p>
+          </body>
         </div>
+       
+        
+        
+       
     )
 }
 
