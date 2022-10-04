ec21c49e129cd6b0e6be417114caf5f489d49b05
B-loeb <bradleyloeb@gmail.com>
2022-10-03 21:17:42 -0400

    old componnent

--- recipe_milestone_project_skeleton/src/App.js
+++ recipe_milestone_project_skeleton/src/App.js
@@ -3,48 +3,45 @@ import Navbar from 'react-bootstrap/Navbar'
 import { Container } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav'
 import './App.css';
+import Recipes from './components/Recipes';
 import Home from './components/Home';
-import AddRecipes from './components/addRecipes';
-import Cuisine from './components/Cuisine';
-import ShowCuisine from './components/ShowCuisine';
 
-export default App;
+
 function App() {
 
   return (
     <div className="App">
       <Router>
-      <div className="App"></div>
-        <Navbar bg="dark" expand="sm" variant="dark">
+        <header>
+      <Navbar bg="dark" expand="lg" variant="dark">
         <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav">
           <Navbar.Brand href="/">
-            <Link to="/" style={{textDecoration: 'none', color: '#ffffcc', display: 'inline-block', fontSize: '1em', textTransform: 'uppercase', padding: '5px', textAlign: 'left', borderRadius: '15%'}}>Home</Link>
+            <Link to="/">Home</Link>
           </Navbar.Brand>
+          
           <Nav className="me-auto" >
             <Nav.Link >
-              <Link to="/Cuisine" style={{textDecoration: 'none', color: '#ffffcc', display: 'inline-block', fontSize: '1.2em', textTransform: 'uppercase', padding: '5px', textAlign: 'left', borderRadius: '15%'}}>Cuisine</Link>
+              <Link to="/Recipes">Recipes</Link>
             </Nav.Link>
-            <Nav.Link>
-              <Link to="/addRecipes" style={{textDecoration: 'none', color: '#ffffcc', display: 'inline-block', fontSize: '1.2em', textTransform: 'uppercase', padding: '5px', textAlign: 'left', borderRadius: '15%'}}>Add Recipes</Link>
-            </Nav.Link>
-            
           </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      
+      </header>
    
+        
+       
       <div className='display'>
         <Routes>
           <Route path='/' element={<Home/>} />
-          <Route path='/addRecipes' element={<AddRecipes/>} />
-          <Route path='/cuisine' element={<Cuisine/>} />
+          <Route path='/Recipes' element={<Recipes/>} />
         </Routes>
       </div>
-      </Router>
-      ;
-</div>) 
-
+      </Router>;
+    </div>
+  );
 }
+
+export default App;
