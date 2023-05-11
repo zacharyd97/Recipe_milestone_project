80f5d9ea0325b863bc2b85c2b2c0a60b9b27c700
Zachary Dowell <zdowell97@gmail.com>
2023-05-11 17:43:42 -0400

    added tailwind

--- recipe_milestone_project_skeleton/src/components/Home.js
+++ recipe_milestone_project_skeleton/src/components/Home.js
@@ -1,7 +1,11 @@
+
+
+
 function Home() {
     return (
         <div className="Home">
-          <h1>Tastes of Home</h1>
+          <h1 className="text-black">Tastes of Home</h1>
+          <p className="text-black">An app to save your favorite recipes</p>
         </div>
        
         
